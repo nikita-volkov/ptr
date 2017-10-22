d56bae2f191e4136ad6a487df56b64911450d51e
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-10-22 16:32:26 +0300

    Make Take bound

--- library/PtrMagic/Take.hs
+++ library/PtrMagic/Take.hs
@@ -1,20 +1,30 @@
 module PtrMagic.Take
 where
 
-import PtrMagic.Prelude hiding (peek)
+import PtrMagic.Prelude hiding (peek, take)
 import qualified PtrMagic.Peek as A
 import qualified Data.ByteString.Char8 as B
 import qualified PtrMagic.Prelude as C
 
 
 newtype Take output =
-  Take (StateT (Ptr Word8) IO output)
+  Take (StateT (Int, Ptr Word8) (MaybeT IO) output)
   deriving (Functor, Applicative, Monad)
+
+{-# INLINE take #-}
+take :: (Int -> Ptr Word8 -> IO (Maybe (a, (Int, Ptr Word8)))) -> Take a
+take io =
+  Take (StateT (\(limitAmount, ptr) -> MaybeT (io limitAmount ptr)))
 
 {-# INLINE peek #-}
 peek :: A.Peek a -> Take a
-peek (A.Peek amount ptrIO) =
-  Take (StateT (\ptr -> fmap (\output -> (output, plusPtr ptr amount)) (ptrIO ptr)))
+peek (A.Peek requiredAmount ptrIO) =
+  Take $ StateT $ \(limitAmount, ptr) -> MaybeT $
+  if requiredAmount <= limitAmount
+    then return Nothing
+    else do
+      result <- ptrIO ptr
+      return (Just (result, (limitAmount - requiredAmount, plusPtr ptr requiredAmount)))
 
 {-# INLINE word8 #-}
 word8 :: Take Word8
@@ -44,16 +54,23 @@ bytes amount =
 {-# INLINE nullTerminatedBytes #-}
 nullTerminatedBytes :: Take ByteString
 nullTerminatedBytes =
-  Take $ StateT $ \ptr ->
-  fmap (\bytes -> (bytes, plusPtr ptr (succ (B.length bytes)))) (B.packCString (castPtr ptr))
+  take $ \limitAmount ptr -> do
+    bytes <- B.packCString (castPtr ptr)
+    case succ (B.length bytes) of
+      consumedAmount -> if consumedAmount <= limitAmount
+        then return (Just (bytes, (limitAmount - consumedAmount, plusPtr ptr consumedAmount)))
+        else return Nothing
 
 {-# INLINE bytesWhile #-}
 bytesWhile :: (Word8 -> Bool) -> Take ByteString
 bytesWhile predicate =
-  Take $ StateT $ \ptr -> do
-    (newPtr, length) <- iterate ptr 0
-    bytes <- B.packCStringLen (castPtr ptr, length)
-    return (bytes, newPtr)
+  take $ \limitAmount ptr -> do
+    (newPtr, consumedAmount) <- iterate ptr 0
+    if consumedAmount <= limitAmount
+      then do
+        bytes <- B.packCStringLen (castPtr ptr, consumedAmount)
+        return (Just (bytes, (limitAmount - consumedAmount, newPtr)))
+      else return Nothing
   where
     iterate !ptr !i =
       do
@@ -65,28 +82,32 @@ bytesWhile predicate =
 {-# INLINE skipWhile #-}
 skipWhile :: (Word8 -> Bool) -> Take ()
 skipWhile predicate =
-  Take $ StateT $ \ptr -> do
-    newPtr <- iterate ptr
-    return ((), newPtr)
+  take $ \limitAmount ptr -> do
+    (newPtr, consumedAmount) <- iterate ptr 0
+    if consumedAmount <= limitAmount
+      then return (Just ((), (limitAmount - consumedAmount, newPtr)))
+      else return Nothing
   where
-    iterate !ptr =
+    iterate !ptr !i =
       do
         byte <- C.peek ptr
         if predicate byte
-          then iterate (plusPtr ptr 1)
-          else return ptr
+          then iterate (plusPtr ptr 1) (succ i)
+          else return (ptr, i)
 
 {-# INLINE foldWhile #-}
 foldWhile :: (Word8 -> Bool) -> (state -> Word8 -> state) -> state -> Take state
 foldWhile predicate step start =
-  Take $ StateT $ \ptr -> iterate ptr start
+  take (iterate start)
   where
-    iterate !ptr !state =
-      do
-        byte <- C.peek ptr
-        if predicate byte
-          then iterate (plusPtr ptr 1) (step state byte)
-          else return (state, ptr)
+    iterate !state !limit !ptr =
+      if limit > 0
+        then do
+          byte <- C.peek ptr
+          if predicate byte
+            then iterate (step state byte) (pred limit) (plusPtr ptr (-1))
+            else return (Just (state, (limit, ptr)))
+        else return Nothing
 
 -- |
 -- Unsigned integral number encoded in ASCII.
