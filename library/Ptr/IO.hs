971c5c3ccb63147be9e07ed037eb16ca2311057c
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-04-05 09:17:58 +0300

    Add left-endian poking IO

--- library/Ptr/IO.hs
+++ library/Ptr/IO.hs
@@ -131,12 +131,18 @@ pokeWord8 ptr value =
   {-# SCC "pokeWord8" #-} 
   poke ptr value
 
+{-# INLINE pokeWord8Off #-}
+pokeWord8Off :: Ptr Word8 -> Int -> Word8 -> IO ()
+pokeWord8Off ptr off value =
+  {-# SCC "pokeWord8Off" #-} 
+  pokeByteOff ptr off value
+
 {-# INLINE pokeBEWord16 #-}
 pokeBEWord16 :: Ptr Word8 -> Word16 -> IO ()
 #ifdef WORDS_BIGENDIAN
 pokeBEWord16 =
   {-# SCC "pokeBEWord16" #-} 
-  poke
+  pokeStorable
 #else
 pokeBEWord16 ptr value =
   {-# SCC "pokeBEWord16" #-} 
@@ -191,6 +197,76 @@ pokeBEWord64 ptr value =
     pokeByteOff ptr 6 (fromIntegral (D.shiftr_w64 value  8) :: Word8)
     pokeByteOff ptr 7 (fromIntegral value :: Word8)
 #endif
+#endif
+
+{-# INLINE pokeLEWord16 #-}
+pokeLEWord16 :: Ptr Word8 -> Word16 -> IO ()
+#ifdef WORDS_BIGENDIAN
+pokeLEWord16 p w =
+  {-# SCC "pokeLEWord16" #-} 
+  do
+    pokeWord8 p (fromIntegral w)
+    pokeWord8Off p 1 (fromIntegral (D.shiftr_w16 w 8))
+#else
+pokeLEWord16 =
+  {-# SCC "pokeLEWord16" #-} 
+  pokeStorable
+#endif
+
+{-# INLINE pokeLEWord32 #-}
+pokeLEWord32 :: Ptr Word8 -> Word32 -> IO ()
+#ifdef WORDS_BIGENDIAN
+pokeLEWord32 p w =
+  {-# SCC "pokeLEWord32" #-}
+  do
+    pokeWord8 p (fromIntegral w)
+    pokeWord8Off p 1 (fromIntegral (D.shiftr_w32 w 8))
+    pokeWord8Off p 2 (fromIntegral (D.shiftr_w32 w 16))
+    pokeWord8Off p 3 (fromIntegral (D.shiftr_w32 w 24))
+#else
+pokeLEWord32 =
+  {-# SCC "pokeLEWord32" #-} 
+  pokeStorable
+#endif
+
+{-# INLINE pokeLEWord64 #-}
+pokeLEWord64 :: Ptr Word8 -> Word64 -> IO ()
+#ifdef WORDS_BIGENDIAN
+#if WORD_SIZE_IN_BITS < 64
+--
+-- To avoid expensive 64 bit shifts on 32 bit machines, we cast to
+-- Word32, and write that
+--
+pokeLEWord64 p w =
+  {-# SCC "pokeLEWord64" #-} 
+  do
+    let b = fromIntegral (D.shiftr_w64 w 32) :: Word32
+        a = fromIntegral w                   :: Word32
+    pokeWord8 p (fromIntegral a)
+    pokeWord8Off p 1 (fromIntegral (D.shiftr_w32 a 8))
+    pokeWord8Off p 2 (fromIntegral (D.shiftr_w32 a 16))
+    pokeWord8Off p 3 (fromIntegral (D.shiftr_w32 a 24))
+    pokeWord8Off p 4 (fromIntegral b)
+    pokeWord8Off p 5 (fromIntegral (D.shiftr_w32 b 8))
+    pokeWord8Off p 6 (fromIntegral (D.shiftr_w32 b 16))
+    pokeWord8Off p 7 (fromIntegral (D.shiftr_w32 b 24))
+#else
+pokeLEWord64 p w =
+  {-# SCC "pokeLEWord64" #-} 
+  do
+    pokeWord8 p (fromIntegral w)
+    pokeWord8Off p 1 (fromIntegral (D.shiftr_w64 w 8))
+    pokeWord8Off p 2 (fromIntegral (D.shiftr_w64 w 16))
+    pokeWord8Off p 3 (fromIntegral (D.shiftr_w64 w 24))
+    pokeWord8Off p 4 (fromIntegral (D.shiftr_w64 w 32))
+    pokeWord8Off p 5 (fromIntegral (D.shiftr_w64 w 40))
+    pokeWord8Off p 6 (fromIntegral (D.shiftr_w64 w 48))
+    pokeWord8Off p 7 (fromIntegral (D.shiftr_w64 w 56))
+#endif
+#else
+pokeLEWord64 =
+  {-# SCC "pokeLEWord64" #-} 
+  pokeStorable
 #endif
 
 {-# INLINE pokeBytesTrimming #-}
