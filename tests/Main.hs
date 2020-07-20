713d8d4e257a4793b79d7cbb8897e1d75288fd19
Nikita Volkov <nikita.y.volkov@mail.ru>
2020-07-20 12:38:34 +0300

    Fix the read of value in the asciiUtcTimeInIso8601 test

--- tests/Main.hs
+++ tests/Main.hs
@@ -43,7 +43,7 @@ main =
         assertEqual "" "000" (A.poking (F.asciiPaddedAndTrimmedIntegral 3 (-1)))
       ,
       testCase "asciiUtcTimeInIso8601" $ do
-        assertEqual "" "2017-02-01T05:03:58Z" (A.poking (F.asciiUtcTimeInIso8601 (read "2017-02-01 05:03:58")))
+        assertEqual "" "2017-02-01T05:03:58Z" (A.poking (F.asciiUtcTimeInIso8601 (read "2017-02-01 05:03:58Z")))
       ,
       testCase "fromString" $ do
         assertEqual "" "123" (A.poking "123")
