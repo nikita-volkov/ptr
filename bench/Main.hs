4fe1c06d0d696249e3f7c635eac46aa25f3743f0
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-03-27 12:39:09 +0300

    Reorder benchmarks

--- bench/Main.hs
+++ bench/Main.hs
@@ -7,13 +7,13 @@ import qualified Data.Serialize as Cereal
 
 main =
   defaultMain [
-      bench "int32InBe*3"
-        $ nf
-          (Read.runOnByteStringFinishing ((,,) <$> Read.int32InBe <*> Read.int32InBe <*> Read.int32InBe))
-          (Cereal.runPut (Cereal.putInt32be 1 <> Cereal.putInt32be 2 <> Cereal.putInt32be 3))
-      ,
       bench "int32InBe"
         $ nf
           (Read.runOnByteStringFinishing (Read.int32InBe))
-          (Cereal.runPut (Cereal.putInt32be 1))
+          $! Cereal.runPut (Cereal.putInt32be 1)
+      ,
+      bench "int32InBe*3"
+        $ nf
+          (Read.runOnByteStringFinishing ((,,) <$> Read.int32InBe <*> Read.int32InBe <*> Read.int32InBe))
+          $! Cereal.runPut (Cereal.putInt32be 1 <> Cereal.putInt32be 2 <> Cereal.putInt32be 3)
     ]
